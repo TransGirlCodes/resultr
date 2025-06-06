644131f3737d54f8a55d41019173ba335592a00f
TRΛNS-GIRL.C0DΞS <me@trans-girl.codes>
2025-06-07 00:53:03 +0100

    Add pkg level docs

---
+++ R/resultr-package.R
@@ -0,0 +1,14 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+# enable usage of <S7_object>@name in package code
+#' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
+NULL
+
+.onLoad <- function(...) {
+  S7::methods_register()
+}
